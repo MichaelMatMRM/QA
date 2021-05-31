c8391574563726f7d9b29e7f96dd523c574e966b
MichaelMatMRM <72271693+MichaelMatMRM@users.noreply.github.com>
2021-06-01 00:09:21 +0300

    Add REST Controller test

---
+++ HashMap.Tests/RestTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+using HashTableShared;
+using REST.Controllers;
+
+namespace HashMap.Tests {
+    public class RestTests {
+
+        [Theory]
+        [InlineData("AA99AA-hello=SS34SD-HI")]
+        public void TestControllerGet(string request) {
+            var table = new HashTable();
+
+            foreach (var pair in request.Split('=')) {
+                var data = pair.Split('-');
+                if (data.Length != 2) continue;
+                table[data[0]] = data[1];
+            }
+
+            var controller = new HashMapController();
+            var map = controller.Get(request);
+
+
+            Assert.Equal(table.Elements, map.Elements);
+        }
+
+    }
+}
